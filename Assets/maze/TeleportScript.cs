7eb6c04c1fb07cc7eeb60da1d773f3dd0abc4c23
BahuMan <bart.huylebroeck@dont.spam.be>
2015-04-22 20:56:26 +0200

    MegaPacMan
    one maze, 1 ghost (red) + pathfinding

---
+++ Assets/maze/TeleportScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportScript : MonoBehaviour {
+
+	public Transform transportTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		other.transform.position = transportTarget.position;
+	}
+}
