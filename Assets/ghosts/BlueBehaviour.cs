2c68e7c2aa5ce85cdb6fed4d254b96e6bcf151b7
BahuMan <bart.huylebroeck@dont.spam.be>
2015-05-05 21:17:42 +0200

    added inky (blue)
    + different strategy for all three ghosts

---
+++ Assets/ghosts/BlueBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlueBehaviour : MonoBehaviour {
+
+	public Transform blinky;
+
+	private Transform player;
+	private NavMeshAgent agent;
+
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindWithTag ("Player").transform;
+		agent = GetComponent<NavMeshAgent> ();	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerpos = player.position;
+		Vector3 blinkypos = blinky.position;
+		//go to the other side of the player:
+		agent.SetDestination ((playerpos - blinkypos) + playerpos);
+	}
+}
